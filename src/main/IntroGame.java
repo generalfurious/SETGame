ab7ec975adf3e82b3122d62f13e651c15500f7cd
stefano <stefano@10.52.161.43>
2014-07-11 16:51:48 +0200

    introgame neu

--- src/main/IntroGame.java
+++ src/main/IntroGame.java
@@ -11,6 +11,7 @@ import javax.swing.JOptionPane;
 
 public class IntroGame extends javax.swing.JFrame {
     //f
+    String a = null;
     ArrayList<String> color = new ArrayList<>();
     ArrayList<String> symbol = new ArrayList<>();
     String checkcolor;
